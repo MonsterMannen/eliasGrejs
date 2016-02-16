5a7be216a9249b3e9c2f489639f2e1d598d7a9f7
MonsterMannen <viktorsvensson94@gmail.com>
2016-02-16 17:15:33 +0100

    adding memberFiles

---
+++ readmembers/Member.java
@@ -0,0 +1,24 @@
+public class Member {
+	//variables
+	private int id;
+	private String name;
+	private String familyName;
+	private String birthDate;
+	private String memberSinceDate;
+	private int role;
+	private String team;
+	private int gender;
+
+	//constructor
+	public Member(int id, String name, String familyName, String birthDate, 
+					String memberSinceDate, int role, String team, int gender){
+		this.id = id;
+		this.name = name;
+		this.familyName = familyName;
+		this.birthDate = birthDate;
+		this.memberSinceDate = memberSinceDate;
+		this.role = role;
+		this.team = team;
+		this.gender = gender;
+	}
+}
